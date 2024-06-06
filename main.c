657014fb1d683d28760544aa22e51216178c9b64
DaniSwi <163067498+DaniSwi@users.noreply.github.com>
2024-06-06 21:49:54 +0000

    Primer struct mal hecho :c

--- main.c
+++ main.c
@@ -1,7 +1,19 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+struct Producto {
+  int codigo;
+  char nombre[1000];
+  float precio;
+};
+
+
 int main() {
+  struct Producto carne;
+  carne.codigo = 123456;
+  strcpy(carne.nombre, "carne");
+  carne.precio = 1000;
+  
   printf("XES BEST GRUPO\n");
   return 0;
 }
