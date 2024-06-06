161fae93895191685bcb380b651716424130ebe3
DaniSwi <163067498+DaniSwi@users.noreply.github.com>
2024-06-06 22:09:47 +0000

    Struct bien hecho? xd

--- main.c
+++ main.c
@@ -1,18 +1,26 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
+
+//ELIMINAR ULTIMO CAMBIO ---> git reset --hard HEAD~1 (vengan a la puta ayudantia cabrones)
 
 struct Producto {
   int codigo;
   char nombre[1000];
   float precio;
+  //Fecha de vencimiento:
+  int dia;
+  int mes;
+  int year;
 };
 
-
 int main() {
-  struct Producto carne;
-  carne.codigo = 123456;
-  strcpy(carne.nombre, "carne");
-  carne.precio = 1000;
+  struct Producto carne; 
+  carne.dia = 12;
+  carne.mes = 4;
+  carne.year = 2025;
+  carne.codigo = 12345678;
+  carne.precio = 10000; //Precio por kilo?!?!?!?
   
   printf("XES BEST GRUPO\n");
   return 0;
